b62279f11920281f2748ac2467d8e61cad677606
JC Gonzalez <devgonzalez25@gmail.com>
2019-05-16 11:58:40 -0500

    Importing all reducers from the the reducers directory. Connecting them to combine reducers

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -6,78 +6,25 @@ const sagaMiddleware = createSagaMiddleware();
 // import * as sagas from './sagas.mock';
 import * as sagas from './sagas';
 import * as mutations from './mutations';
+import {userInterface} from './Reducers/UserInterface'
+import {comments} from './Reducers/Comments'
+import {students} from './Reducers/Students'
+import {student} from './Reducers/Student'
+import {users} from './Reducers/Users'
+import {groups} from './Reducers/Groups'
+import {session} from './Reducers/Session'
+
+
 
 export const store = createStore(
     combineReducers({
-        userInterface(userInterface = {formState:"login"},action){
-            switch(action.type){
-                case mutations.FORM_UPDATE:
-                    return {...userInterface, formState:action.changeTo};
-            }
-            return userInterface;
-        },
-        session(userSession=[] || {},action){
-            let {type,status} = action;
-            userSession = userSession || action.session;
-            switch(type){
-                case mutations.SET_STUDENT_STATE:
-                    return {...userSession,...action.state.session}
-                case mutations.SET_STATE:
-                    return {...userSession,id:action.state.session.id}
-                case mutations.REQUEST_AUTHENTICATE_USER:
-                    return {...userSession, authenticated:mutations.AUTHENTICATING};
-                case mutations.PROCESSING_AUTHENTICATE_USER:
-                    return {...userSession, authenticated:status};
-                case mutations.REGISTRATION_PROCESSING:
-                    return {...userSession,registration_status:status};
-                default:
-                    return userSession;
-            }
-
-        },
-        student(student={},action){
-            switch(action.type){
-                case mutations.SET_STUDENT_STATE:
-                    return {...action.state};
-            }
-            return student;
-        },
-        students(students=[],action){
-            switch(action.type){
-                case mutations.SET_STATE:
-                    return action.state.students;
-                case mutations.CREATE_TASK:
-                    return [...tasks,
-                        {
-                            name:action.name,
-                            id:action.taskID,
-                            group:action.groupID,
-                            owner:action.ownerID,
-                            iscomplete:false,
-                        }
-                    ]
-                case mutations.SET_COMPLETION:
-                    return students.map(task=>task.id==action.id?{...student,isComplete:action.isComplete}:student);
-                case mutations.SET_STUDENT_GROUP:
-                    return students.map(student=>student.id==action.id?{...student,group:action.group}:student);
-                case mutations.SET_STUDENT_MESSAGE:
-                    return students.map(student=>student.id==action.id?{...student,message:action.message}:student);
-            }
-            return students;
-        },
-        comments(comments = [],action){
-                return comments;
-        },
-        groups(groups = [],action){
-            switch(action.type){
-                case mutations.SET_STATE:
-                    return action.state.groups;
-            }
-            return groups
-        },
-        users(users = []){
-            return users;
-        }
+        userInterface,
+        session,
+        student,
+        students,
+        comments,
+        groups,
+        users
     })
     ,applyMiddleware(createLogger(),sagaMiddleware)
 )
